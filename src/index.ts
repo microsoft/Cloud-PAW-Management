95d10ef2777282480d3ce2f4d5c8a6323fbc17ca
Elliot Huffman <ehuffman@elliot-labs.com>
2020-12-31 10:35:29 -0500

    Initial login logic
    Create logic to automatically log in the app depending on the environment.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { ManagedIdentityCredential, VisualStudioCodeCredential } from "@azure/identity"
+
+let credential: ManagedIdentityCredential | VisualStudioCodeCredential
+
+if (process.env.ENVIRONMENT === "Prod") {
+    credential = new ManagedIdentityCredential()
+} else {
+    credential = new VisualStudioCodeCredential()
+}
