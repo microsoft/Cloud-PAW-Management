cd7b06608db5675ef3c2026059ec1474b2939a60
Elliot Huffman <ehuffman@elliot-labs.com>
2020-12-31 18:16:11 -0500

    Initial build of the graph client
    Create the initial graph client. This initial build of the clint allows for the login of the client. it will return a client instance but that will be changed to support a class based structure to help organize and secure the data.

---
+++ src/GraphClient.ts
@@ -0,0 +1,28 @@
+import { Client, ClientOptions } from "@microsoft/microsoft-graph-client";
+import type { ClientSecretCredential } from "@azure/identity"
+import { GraphClientAuthProvider } from "./Authentication";
+import "isomorphic-fetch";
+
+export class MSGraphClient {
+    private configurationList: Array<any> = []
+
+    // Define the login command that returns a connected instance of the Graph client
+    login(credentials: ClientSecretCredential): Client {
+        // Instantiate the access token interpreter
+        const graphAuthProvider = new GraphClientAuthProvider(credentials);
+
+        // Configure teh initialization system to use the custom graph auth provider
+        const clientOptions: ClientOptions = {
+            // Configure the auth provider property to be the value of the graph auth constant
+            authProvider: graphAuthProvider
+        };
+
+        // connect the graph client to the graph
+        return Client.initWithMiddleware(clientOptions);
+    }
+
+    // retrieve a list of all device configurations that are accessible to the app
+    getDeviceConfigList() {
+        console.log(this.configurationList)
+    }
+}
