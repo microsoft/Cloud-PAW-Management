1da6aa384956950c5c11f44b3a95d0d2c223e90f
Elliot Huffman <ehuffman@elliot-labs.com>
2020-12-31 11:58:22 -0500

    Implement Azure Login
    Create the Azure login process to automatically log into the Azure fabric.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
-import { Express } from "express"
-import { ManagedIdentityCredential, VisualStudioCodeCredential } from "@azure/identity"
+import express from "express";
+import { Client } from "@microsoft/microsoft-graph-client";
+import { DefaultAzureCredential, DefaultAzureCredentialOptions } from "@azure/identity";
 
-let credential: ManagedIdentityCredential | VisualStudioCodeCredential
+// Import environmental variables
+const port = process.env.PORT || 3000;
+const userAssignedManagedIdentityID = process.env.MANAGED_IDENT_GUID || "none";
 
-if (process.env.ENVIRONMENT === "Prod") {
-    credential = new ManagedIdentityCredential()
+// Check if a user assigned managed identity is specified
+if (userAssignedManagedIdentityID !== "none") {
+    // if one is, build the configuration for the auto login to use it
+    const credOptions: DefaultAzureCredentialOptions = {
+        managedIdentityClientId: userAssignedManagedIdentityID
+    };
+    // log in with the user assigned managed identity 
+    const credential = new DefaultAzureCredential(credOptions);
+// Otherwise use the default configuration for system assigned identities
 } else {
-    credential = new VisualStudioCodeCredential()
-}
+    // auto log in with default configuration
+    const credential = new DefaultAzureCredential();
+};
+
+// Initialize the Microsoft Graph client
+
+
+// Initialize Express
+const webServer = express();
+
+// Configure Express
+
+// Start the web server
+const serverInstance = webServer.listen(port, ()=> {
+    console.log("Server running on port: " + port);
+});
