c9c29314f05834efc412ee03f3e7c70852bd706b
Elliot Huffman <ehuffman@elliot-labs.com>
2020-12-31 13:01:30 -0500

    Move auth into its own class
    Convert the hard coded auth code to its own class to help with modularization and code cleanlyness.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
 import express from "express";
-import { Client } from "@microsoft/microsoft-graph-client";
-import { DefaultAzureCredential, DefaultAzureCredentialOptions } from "@azure/identity";
+import { MSAzureAccessCredential } from "./Authentication"
 
 // Import environmental variables
 const port = process.env.PORT || 3000;
 const userAssignedManagedIdentityID = process.env.MANAGED_IDENT_GUID || "none";
 
-// Check if a user assigned managed identity is specified
-if (userAssignedManagedIdentityID !== "none") {
-    // if one is, build the configuration for the auto login to use it
-    const credOptions: DefaultAzureCredentialOptions = {
-        managedIdentityClientId: userAssignedManagedIdentityID
-    };
-    // log in with the user assigned managed identity 
-    const credential = new DefaultAzureCredential(credOptions);
-// Otherwise use the default configuration for system assigned identities
-} else {
-    // auto log in with default configuration
-    const credential = new DefaultAzureCredential();
-};
+// Log into Azure AD
+const credential = new MSAzureAccessCredential(userAssignedManagedIdentityID);
 
 // Initialize the Microsoft Graph client
 
@@ -27,6 +15,8 @@ if (userAssignedManagedIdentityID !== "none") {
 const webServer = express();
 
 // Configure Express
+
+// Define the management API
 
 // Start the web server
 const serverInstance = webServer.listen(port, ()=> {
