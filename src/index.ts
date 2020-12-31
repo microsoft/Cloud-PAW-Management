259ca5b3f76cbe10ff46738ff9ea2eab75bcc0fd
Elliot Huffman <ehuffman@elliot-labs.com>
2020-12-31 18:17:49 -0500

    recode for graph client
    Remove managed identity auth and add support for the Microsoft Graph API client which is exposed initially through a class structure.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import express from "express";
 import { MSAzureAccessCredential } from "./Authentication"
+import { MSGraphClient } from "./GraphClient";
 
 // Import environmental variables
+const clientID = process.env.Client_GUID || "None"
+const clientSecret = process.env.Client_Secret || "None"
+const tenantID = process.env.Tenant_ID || "None"
 const port = process.env.PORT || 3000;
-const userAssignedManagedIdentityID = process.env.MANAGED_IDENT_GUID || "none";
+
+// Validate environmental variable input to ensure that the input is as expected
+if (clientID === "None") {
+    throw new Error("Client ID is not configured!");
+};
+if (clientSecret === "None") {
+    throw new Error("Client Secret is not configured!");
+};
+if (tenantID === "None") {
+    throw new Error("Tenant ID is not configured!");
+};
 
 // Log into Azure AD
-const credential = new MSAzureAccessCredential(userAssignedManagedIdentityID);
+const azureAuthSession = new MSAzureAccessCredential(clientID, clientSecret, tenantID);
 
 // Initialize the Microsoft Graph client
-
+const graphClient = new MSGraphClient();
+const instance = graphClient.login(azureAuthSession.credential);
 
 // Initialize Express
 const webServer = express();
@@ -19,6 +34,6 @@ const webServer = express();
 // Define the management API
 
 // Start the web server
-const serverInstance = webServer.listen(port, ()=> {
+const serverInstance = webServer.listen(port, () => {
     console.log("Server running on port: " + port);
 });
