657becd735c86354b290337e5d9c7c1f79f31565
Elliot Huffman <ehuffman@elliot-labs.com>
2020-12-31 18:15:07 -0500

    Create Auth Provider for Graph Client
    Create an authentication provider for the Graph API client. This is requried for the conversion of the @Azure-Identity objects to raw access tokens which are needed for the graph api client.

--- src/Authentication.ts
+++ src/Authentication.ts
@@ -9,3 +9,41 @@ export class MSAzureAccessCredential {
         this.credential = new ClientSecretCredential(tenantID, appRegID, appRegSecret);
     };
 };
+
+export class GraphClientAuthProvider implements AuthenticationProvider {
+    // define the credential property as the get access token method is not allowed to have parameters
+    // data will have to be passed into it via property access instead of param access.
+    // this property should be private as nobody needs to access the auth system besides the graph client.
+    private credential: ClientSecretCredential
+
+    // Create the credential constructor
+    constructor(AzureIdentityCredential: ClientSecretCredential){
+        // Set the value of the credential property with the azure credential passed to it
+        this.credential = AzureIdentityCredential;
+    }
+
+    // Implement the access token retrieval logic as required by the graph client custom auth provider.
+    // This method must return an access token.
+	public async getAccessToken(): Promise<string> {
+        // Create a promise to process the retrieval of the access token
+        const AccessToken: Promise<string> = new Promise((resolve,reject)=> {
+            // Retrieve the access token for the Microsoft Graph default scope (defined by the AAD app registration)
+            this.credential.getToken("https://graph.microsoft.com/.default").then((token)=> {
+                // if no token is retrieved, reject the promise as it has failed.
+                if (token === null) {
+                    reject("No token retrieved")
+                // Otherwise resolve the promise with the raw token string
+                } else {   
+                    resolve(token.token)
+                }
+            // catch any other uncaught errors
+            }).catch((error)=>{
+                // reject the promise with the other uncaught error's data
+                reject(error);
+            });
+        });
+
+        // return the access token promise
+        return AccessToken;
+    }
+}
