9d58b7b6df278b539a99219de1b3cef10d168e62
Elliot Huffman <ehuffman@elliot-labs.com>
2020-12-31 18:13:59 -0500

    Recode for AAD App Reg
    Recode the app to allow for the manual configuration fo the AAD Application Registration based secret authentication.

--- src/Authentication.ts
+++ src/Authentication.ts
@@ -1,23 +1,11 @@
-import { DefaultAzureCredential, DefaultAzureCredentialOptions } from "@azure/identity"
-
+import { ClientSecretCredential } from "@azure/identity"
+import type { AuthenticationProvider } from "@microsoft/microsoft-graph-client";
 export class MSAzureAccessCredential {
     // strictly type the credential property
-    credential: DefaultAzureCredential
+    credential: ClientSecretCredential
 
-    // Define the authentication logic class initialization, optionally taking a GUID that points to a user assigned managed identity
-    constructor(userAssignedManagedIdentityID?: string) {
-        // Check if a user assigned managed identity is specified
-        if (userAssignedManagedIdentityID !== "none" && typeof userAssignedManagedIdentityID !== "undefined") {
-            // if one is, build the configuration for the auto login to use it
-            const credOptions: DefaultAzureCredentialOptions = {
-                managedIdentityClientId: userAssignedManagedIdentityID
-            };
-            // log in with the user assigned managed identity 
-            this.credential = new DefaultAzureCredential(credOptions);
-            // Otherwise use the default configuration for system assigned identities
-        } else {
-            // auto log in with default configuration
-            this.credential = new DefaultAzureCredential();
-        };
+    // todo: write docs and validate, add managed identity support
+    constructor(appRegID: string, appRegSecret: string, tenantID: string) {
+        this.credential = new ClientSecretCredential(tenantID, appRegID, appRegSecret);
     };
 };
