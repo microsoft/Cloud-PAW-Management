7b9290a905b211a7055d5d9424ed6d3715d9c345
Elliot Huffman <ehuffman@elliot-labs.com>
2021-01-01 20:24:20 -0500

    Allow any object input
    All the input to be any object. After validating that the string is not undefined or null, it is then sent to the toString method to convert it to a string. After it is a string it is then validated.
    This allows the validation function to be used in more than just string only situations.

--- src/Utility.ts
+++ src/Utility.ts
@@ -1,14 +1,20 @@
 // Create a GUID validation function to ensure GUID data is in correct format
-export function validateGUID(GUIDToTest: string): boolean {
-    // Take the input and run a GUID string regex match against it and store the results.
-    const results = GUIDToTest.match("/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i")
-
-    // Check to see if any match was found and if a match was found, check to make sure it was only one.
-    if (results !== null && results.length === 1) {
-        // If a match was found, we have a valid GUID and should return true
-        return true
+export function validateGUID(GUIDToTest: any): boolean {
+    // If the data is undefined, it is not a GUID
+    if (typeof GUIDToTest === "undefined" || GUIDToTest === null) {
+        // Return false since it is not a GUID
+        return false;
     } else {
-        // If no or more than one matches were found, return false as it is not a valid single GUID.
-        return false
-    }
-}
+        // Convert the data to a string then run a GUID string regex match against it and store the results.
+        const results = GUIDToTest.toString().match("/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i")
+    
+        // Check to see if any match was found and if a match was found, check to make sure it was only one.
+        if (results !== null && results.length === 1) {
+            // If a match was found, we have a valid GUID and should return true
+            return true;
+        } else {
+            // If no or more than one matches were found, return false as it is not a valid single GUID.
+            return false;
+        };
+    };
+};
