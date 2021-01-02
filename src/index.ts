7b07f486c7bfb309c2a7a7acbcdf904e0c02b6ce
Elliot Huffman <ehuffman@elliot-labs.com>
2021-01-02 13:26:53 -0500

    Add Debug Mode
    Add support for debug mode so that it can be toggled on by the end admin or a dev.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { MSAzureAccessCredential } from "./Authentication"
 
 // Import environmental variables
 const port = process.env.PORT || 3000;
+const debugMode = process.env.Debug || "false"
 
 // Check to see if a user assigned managed identity GUID is provided.
 // If it is, log into Azure with user assigned MI and app registration credentials.
@@ -15,30 +16,33 @@ const azureAuthSession = new MSAzureAccessCredential();
 // Initialize Express
 const webServer = express();
 
-// Debugging entry to list access token to manually web request as the app
-webServer.get('/accessToken', async (request, response) => {
-    try {
-        // grab a token and extract its value
-        const token = await (await azureAuthSession.credential).getToken("https://graph.microsoft.com/.default");
+// If debug mode is enabled, add additional routes
+if (debugMode === "true") {
+    // List access token to manually web request as the app
+    webServer.get('/accessToken', async (request, response) => {
+        try {
+            // grab a token and extract its value
+            const token = await (await azureAuthSession.credential).getToken("https://graph.microsoft.com/.default");
 
-        // Validate that the token has value
-        if (token !== null) {
-            // If it does, send its value as a response
-            response.send(token);
-            // If it does not
-        } else {
-            // Send a notice to the caller stating that it does not have value.
-            response.send("no token data received")
-        };
-    } catch (error) {
-        response.send(error);
-    }
-});
+            // Validate that the token has value
+            if (token !== null) {
+                // If it does, send its value as a response
+                response.send(token);
+                // If it does not
+            } else {
+                // Send a notice to the caller stating that it does not have value.
+                response.send("no token data received")
+            };
+        } catch (error) {
+            response.send(error);
+        }
+    });
 
-// Send all environmental vars
-webServer.get('/envVar', async (request, response) => {
-    response.send(process.env)
-});
+    // Send all environmental vars
+    webServer.get('/envVar', async (request, response) => {
+        response.send(process.env)
+    });
+};
 
 // Start the web server
 const serverInstance = webServer.listen(port, () => {
