61e6651002f56bbcd58fe9fb1e7667f5eb3e9888
Elliot Huffman <elliot.huffman@microsoft.com>
2021-06-05 17:49:41 -0400

    Comments
    Update the comments to be correct, the previous comments were incorrectly labeled as GUID and not for email.
    Update wording on the existing comment for the GUID test results return as it was for the old test style.

--- src/Utility.ts
+++ src/Utility.ts
@@ -8,22 +8,22 @@ export function validateGUID(GUIDToTest: any): boolean {
         // Define the GUID pattern
         const GUIDRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/gi;
 
-        // Test the value to ensure it is in the right format
+        // Test the value to ensure it is in the right format and return the results
         return GUIDRegex.test(GUIDToTest.toString());
     };
 };
 
-// Create a GUID validation function to ensure GUID data is in correct format
+// Validate the specified email address to ensure it is in the correct format
 export function validateEmail(emailToTest: any): boolean {
-    // If the data is undefined, it is not a GUID
+    // If the data is undefined, it is not an email
     if (typeof emailToTest === "undefined" || emailToTest === null) {
-        // Return false since it is not a GUID
+        // Return false since it is not an email
         return false;
     } else {
-        // Define the GUID pattern
+        // Define the email regex pattern
         const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-        // Test the value to ensure it is in the right format
+        // Test the value to ensure it is in the right format and return the results
         return emailRegex.test(emailToTest.toString());
     };
 };
