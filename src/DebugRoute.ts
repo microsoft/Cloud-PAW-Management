a32003b6ee9ffe1031f5680cf4b66388aae097b9
Elliot Huffman <elliot.huffman@microsoft.com>
2021-06-04 20:45:52 -0400

    Move Debug Routes
    Move the debug routes into their own file to de-clutter the main file.
    This also makes it easier to manage the routes.

---
+++ src/DebugRoute.ts
@@ -0,0 +1,69 @@
+import type { MSGraphClient } from "./GraphClient";
+import type express from "express";
+import type { ChainedTokenCredential } from "@azure/identity"
+
+export class DebugRouter {
+    // Define the properties that will be available to the class
+    private webServer: express.Express;
+    private roleScopeTag: String | undefined;
+    private graphClient: MSGraphClient;
+
+    // Define how the class should be instantiated
+    constructor(webServer: express.Express, graphClient: MSGraphClient, credential: Promise<ChainedTokenCredential>) {
+        // Initialize the environmental variable list
+        this.roleScopeTag = process.env.Scope_Tag
+        
+        // Make the express instance available to the class
+        this.webServer = webServer;
+
+        // Make the graph client instance available to the class
+        this.graphClient = graphClient;
+
+        // Initialize the routes
+        this.init(credential);
+    }
+
+    // Initialize the routes
+    private init(azureAuthSession: Promise<ChainedTokenCredential>) {
+        // List access token to manually web request as the app
+        this.webServer.get('/accessToken', async (request, response) => {
+            try {
+                // grab a token and extract its value
+                const token = await (await azureAuthSession).getToken("https://graph.microsoft.com/.default");
+
+                // Validate that the token has value
+                if (token !== null) {
+                    // If it does, send its value as a response
+                    response.send(token);
+                    // If it does not
+                } else {
+                    // Send a notice to the caller stating that it does not have value.
+                    response.send("no token data received")
+                };
+            } catch (error) {
+                response.send(error);
+            }
+        });
+
+        // Send all environmental vars
+        this.webServer.get('/envVar', (request, response) => {
+            response.send(process.env)
+        });
+
+        // Configure the role scope tag endpoint to return the configured role scope tag
+        this.webServer.get('/getRoleScopeTag', async (request, response) => {
+            if (typeof this.roleScopeTag === "undefined") {
+                // Notify the calling app that the role scope tag is not defined in the env vars.
+                response.send("The role scope tag variable is not defined!");
+            } else {
+                // Get all role scope tags in Microsoft Endpoint Manager (Intune)
+                response.send(await this.graphClient.getEndpointScopeTag(this.roleScopeTag));
+            }
+        });
+
+        // Configure a debug endpoint that lists all of the role scope tags that are present in the graph API.
+        this.webServer.get('/listRoleScopeTag', async (request, response) => {
+            response.send(await this.graphClient.listEndpointScopeTag());
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { MSAzureAccessCredential } from "./Authentication";
 import { MSGraphClient } from "./GraphClient";
+import { DebugRouter } from "./DebugRoute"
 
 // Import environmental variables
 const port = process.env.PORT || 3000;
 const debugMode = process.env.Debug || "false"
-const roleScopeTag = process.env.Scope_Tag;
 
 // Generate an authentication session that can create access tokens.
 // This will automatically use available credentials available in Managed Identity, Key Vault or environmental vars.
@@ -17,49 +17,11 @@ const graphClient = new MSGraphClient(azureAuthSession.credential);
 // Initialize Express
 const webServer = express();
 
-// If debug mode is enabled, add additional routes
+// If debug mode is enabled, enable the debug routes
 if (debugMode === "true") {
-    // List access token to manually web request as the app
-    webServer.get('/accessToken', async (request, response) => {
-        try {
-            // grab a token and extract its value
-            const token = await (await azureAuthSession.credential).getToken("https://graph.microsoft.com/.default");
-
-            // Validate that the token has value
-            if (token !== null) {
-                // If it does, send its value as a response
-                response.send(token);
-                // If it does not
-            } else {
-                // Send a notice to the caller stating that it does not have value.
-                response.send("no token data received")
-            };
-        } catch (error) {
-            response.send(error);
-        }
-    });
-
-    // Send all environmental vars
-    webServer.get('/envVar', (request, response) => {
-        response.send(process.env)
-    });
-
-    // Configure the role scope tag endpoint to return the configured role scope tag
-    webServer.get('/getRoleScopeTag', async (request, response) => {
-        if (typeof roleScopeTag === "undefined") {
-            // Notify the calling app that the role scope tag is not defined in the env vars.
-            response.send("The role scope tag variable is not defined!");
-        } else {
-            // Get all role scope tags in Microsoft Endpoint Manager (Intune)
-            response.send(await graphClient.getEndpointScopeTag(roleScopeTag));
-        }
-    });
-
-    // Configure a debug endpoint that lists all of the role scope tags that are present in the graph API.
-    webServer.get('/listRoleScopeTag', async (request, response) => {
-        response.send(await graphClient.listEndpointScopeTag());
-    });
-};
+    // Instantiate an instance of the debug router which will add of the debugging routes
+    const debugRoutes = new DebugRouter(webServer, graphClient, azureAuthSession.credential);
+}
 
 // Start the web server
 const serverInstance = webServer.listen(port, () => {
