53feafc52995caaec9511d7d46e5e51f7e7353fe
Elliot Huffman <elliot.huffman@microsoft.com>
2021-06-05 17:42:31 -0400

    Refactor the GUID Validator
    Refactor the GUID validator so that it is a much more simple function. This will make it easier to troubleshoot and more TypeScript friendly.

--- src/Utility.ts
+++ src/Utility.ts
@@ -5,17 +5,13 @@ export function validateGUID(GUIDToTest: any): boolean {
         // Return false since it is not a GUID
         return false;
     } else {
-        // Convert the data to a string then run a GUID string regex match against it and store the results.
-        const results = GUIDToTest.toString().match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$")
-    
-        // Check to see if any match was found and if a match was found, check to make sure it was only one.
-        if (results !== null && results.length === 1) {
-            // If a match was found, we have a valid GUID and should return true
-            return true;
-        } else {
-            // If no or more than one matches were found, return false as it is not a valid single GUID.
-            return false;
-        };
+        // Define the GUID pattern
+        const GUIDRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/gi;
+
+        // Test the value to ensure it is in the right format
+        return GUIDRegex.test(GUIDToTest.toString());
+    };
+};
     };
 };
 
