645f20f6e4e1cde64f5d11c77ab687cb4a60da65
Elliot Huffman <ehuffman@elliot-labs.com>
2021-09-27 23:46:13 -0400

    New Deployment Route
    Create a deployment route for the core groups and scope tag.
    Integrate the Config Engine into the deployment engine.

--- Server/src/DeploymentEngine.ts
+++ Server/src/DeploymentEngine.ts
@@ -2,7 +2,8 @@
 // Licensed under the MIT license.
 
 import type { MSGraphClient } from "./GraphClient";
-import { validateGUIDArray, parseScopeTag, ScopeTagDataIncomplete, writeDebugInfo, InternalAppError } from "./Utility";
+import type { ConfigurationEngine } from "./ConfigEngine";
+import { writeDebugInfo, InternalAppError } from "./Utility";
 import type express from "express";
 import type * as MicrosoftGraphBeta from "@microsoft/microsoft-graph-types-beta";
 
@@ -10,9 +11,10 @@ class DeploymentEngineRouter {
     // Define the properties that will be available to the class
     private webServer: express.Express;
     private graphClient: MSGraphClient;
+    private configEngine: ConfigurationEngine;
 
     // Define how the class should be instantiated
-    constructor(webServer: express.Express, graphClient: MSGraphClient) {
+    constructor(webServer: express.Express, graphClient: MSGraphClient, configEngine: ConfigurationEngine) {
 
         // Make the express instance available to the class
         this.webServer = webServer;
@@ -20,11 +22,49 @@ class DeploymentEngineRouter {
         // Make the graph client instance available to the class
         this.graphClient = graphClient;
 
+        // Make the config engine instance available to the class
+        this.configEngine = configEngine;
+
         // Initialize the routes
         this.initRoutes();
     };
 
+    // Initialize the web server's deployment routes
     private initRoutes(): void {
-        // API stuff here
-    }
+        // Trigger the core security group deployment
+        this.webServer.post("/deploy/core", async (request, response, next) => {
+            try {
+                // Send the boolean response of the deployment operation
+                response.send(await this.configEngine.deployConfig(request.body.userConcent));
+            } catch (error) {
+                // Check to see if the error is an error
+                if (error instanceof InternalAppError) {
+                    if (error.name === "Invalid Input") {
+                        // Write debug info.
+                        writeDebugInfo(error, "Input Validation Error:");
+
+                        // Send a note back to the client about input.
+                        response.send("The expected input is not valid: " + error.message);
+                    // Since our errors are safe, return it to the caller
+                    } else {
+                        // Write debug info
+                        writeDebugInfo(error, "One of our other errors:");
+
+                        // Send the error details back
+                        response.send("Unhandled custom internal error: " + error.name);
+                    }
+                // If the error is not our error, it is unhandled as we didn't control the error
+                } else {
+                    // Write debug info
+                    writeDebugInfo(error, "Unhandled error:");
+
+                    // Send a sanitized generic error back to the caller.
+                    response.send("An unknown error occurred!");
+                };
+            };
+        });
+
+        // Place holder for the core infrastructure single click to deploy method
+        this.webServer.post("/deploy", async (request, response, next) => {});
+    };
 };
