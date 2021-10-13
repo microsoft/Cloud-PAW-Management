0af21ec38e7470d7c9ea6ac75e8a2d0b96d0ab9d
Musema Hassen <muhassen@microsoft.com>
2021-10-13 15:55:17 -0700

    Disable commission PAW button until device is selected

--- User-Interface/src/components/CommissionPawsPanel/CommissionPawsPanelFooter/CommissionPawsPanelFooter.tsx
+++ User-Interface/src/components/CommissionPawsPanel/CommissionPawsPanelFooter/CommissionPawsPanelFooter.tsx
@@ -1,15 +1,21 @@
 import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/components/Button';
 import { IStackTokens, Stack } from '@fluentui/react/lib/components/Stack';
 import React from 'react';
+import { RootStateOrAny, useSelector } from 'react-redux';
 
 export const CommissionPawsPanelFooter = () => {
     const stackTokens: Partial<IStackTokens> = { childrenGap: 20 };
+    const selectedItems = useSelector((state: RootStateOrAny) => state.paw.commissionPaws.devicesToCommission);
+
     const dismissPanel = () => {
 
     };
     return (
         <Stack tokens={stackTokens} horizontal>
-            <PrimaryButton onClick={dismissPanel}>
+            <PrimaryButton
+                onClick={dismissPanel}
+                disabled={!selectedItems || selectedItems.length <= 0 }
+                >
                 Commission PAW
             </PrimaryButton>
             <DefaultButton onClick={dismissPanel}>Cancel</DefaultButton>
