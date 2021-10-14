35718d96f864312f412db9135bfaad7318b0585d
Musema Hassen <muhassen@microsoft.com>
2021-10-14 13:03:44 -0700

    Implement decommission flow

--- User-Interface/src/services/pawService.ts
+++ User-Interface/src/services/pawService.ts
@@ -1,6 +1,6 @@
 import dateformat from 'dateformat';
 import { IDeviceItem, IPawItem } from "../models";
-// import { paws } from './mocks/pawMocks';
+import { paws } from './mocks/pawMocks';
 
 export interface IPawService {
     getPaws: () => Promise<IPawItem[]>,
@@ -10,30 +10,30 @@ export interface IPawService {
 export class PawService {
     public static API_BASE_URL = document.location.origin;
     public static async getPaws(): Promise<IPawItem[]> {
-        const getPawsUrl = `${this.API_BASE_URL}/API/Lifecycle/PAW`
-        const response = await fetch(getPawsUrl);
-        const result = await response.json();
-        return result.map((paw) => {
-            return {
-                displayName: paw.DisplayName,
-                pawId: paw.id,
-                pawType: paw.Type,
-                commissionDate: dateformat(paw.CommissionedDate,'yyyy/mm/dd H:mm'),
-                parentDeviceId: paw.ParentDevice,
-            };   
-        });
-        /*
-        uncomment the below code to work with the mock, and also uncomment paws mock import
-        */
-        // return paws.map((paw) => {
+        // const getPawsUrl = `${this.API_BASE_URL}/API/Lifecycle/PAW`
+        // const response = await fetch(getPawsUrl);
+        // const result = await response.json();
+        // return result.map((paw) => {
         //     return {
         //         displayName: paw.DisplayName,
         //         pawId: paw.id,
         //         pawType: paw.Type,
         //         commissionDate: dateformat(paw.CommissionedDate,'yyyy/mm/dd H:mm'),
         //         parentDeviceId: paw.ParentDevice,
-        //     };
+        //     };   
         // });
+        /*
+        uncomment the below code to work with the mock, and also uncomment paws mock import
+        */
+        return paws.map((paw) => {
+            return {
+                displayName: paw.DisplayName,
+                pawId: paw.id,
+                pawType: paw.Type,
+                commissionDate: dateformat(paw.CommissionedDate,'yyyy/mm/dd H:mm'),
+                parentDeviceId: paw.ParentDevice,
+            };
+        });
     }
     public static commissionPaw = async (deviceItems: IDeviceItem[], pawTypeToCommission: string) => {
         for(const deviceItem of deviceItems) {
