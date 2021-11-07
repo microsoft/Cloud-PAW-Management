b374bd1b31f15c1e284bc15911359cf976d6617a
Elliot Huffman <ehuffman@elliot-labs.com>
2021-11-07 00:56:08 -0400

    Move Redux Store
    Move the redux store to be initialized at the true root of the code instead of inside the app react class. This guarantees that the store is available globally.

--- User-Interface/src/App.tsx
+++ User-Interface/src/App.tsx
@@ -1,18 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { Provider } from 'react-redux';
-import { PawLanding } from './components/PawLanding';
-import { configureStore } from './store/configureStore';
 
-const store = configureStore();
+import { PawLanding } from './components/PawLanding';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <PawLanding />
-      </div>
-    </Provider>
+    <div className="App">
+      <PawLanding />
+    </div>
   );
 }
 
--- User-Interface/src/index.tsx
+++ User-Interface/src/index.tsx
@@ -3,12 +3,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import { store } from './store/configureStore';
 
+// Start the react renderer with strict mode enabled for the whole app
+// Also adds a redux store to the root of the app so that a single global store is available
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
