563d2fd79af5b41c81f1d1680316f23039e12135
Elliot Huffman <ehuffman@elliot-labs.com>
2022-02-03 23:01:01 -0500

    Update App Core
    Update the app core to support react routing and add initial routes to the core app so it knows how to handle various UI endpoints.

--- User-Interface/src/App.tsx
+++ User-Interface/src/App.tsx
@@ -1,13 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { PawLanding } from './components/PawLanding';
+import { Home } from "./components/Home";
+import { DeviceContainer } from "./components/DeviceContainer";
+import { DeviceDetails } from "./components/DeviceDetails";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <PawLanding />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/devices" element={<DeviceContainer />} />
+          <Route path="/devices/:id" element={<DeviceDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
