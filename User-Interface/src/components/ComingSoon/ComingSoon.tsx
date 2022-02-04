a5579406498c6fa9defda89dc8918234ea99d26b
Elliot Huffman <ehuffman@elliot-labs.com>
2022-02-03 22:48:07 -0500

    Initial coming soon page
    Add an initial coming soon page. Not yet used. lol, it is *coming soon* :-P

---
+++ User-Interface/src/components/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// Render a splash screen stating that the feature is coming soon!
+export function ComingSoon() {
+    return (
+        <h1>Coming Soon!</h1>
+    )
+}
